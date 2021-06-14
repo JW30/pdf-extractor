60623bc72770981fa16fd028214109373cef3828
JW30 <46195508+JW30@users.noreply.github.com>
2021-06-14 15:24:08 +0200

    try to fix processing

--- processing.py
+++ processing.py
@@ -4,8 +4,9 @@ import re
 
 def get_numbers(file_path):
     output = ''
-    pdf_string = str(textract.process(file_path))
-    numbers_list = re.sub('\D', ' ', pdf_string).split()
+    pdf_content = textract.process(file_path, encoding="utf-8")
+    text = str(pdf_content)
+    numbers_list = re.sub('\D', ' ', text).split()
     for x in numbers_list:
         if len(x) == 8 and x not in output:
             output += f'{x} '
