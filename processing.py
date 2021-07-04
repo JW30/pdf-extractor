84370bef5c345a3ad5a1d071f54f17d1a458617b
JW30 <46195508+JW30@users.noreply.github.com>
2021-07-04 12:22:08 +0200

    fixed UnicodeDecodeError

--- processing.py
+++ processing.py
@@ -4,7 +4,10 @@ import re
 
 def get_numbers(file_path):
     output = ''
-    pdf_content = textract.process(file_path, encoding="utf-8")
+    try:
+        pdf_content = textract.process(file_path)
+    except UnicodeDecodeError:
+        pdf_content = textract.process(file_path, method="pdfminer")
     text = str(pdf_content)
     numbers_list = re.sub('\D', ' ', text).split()
     for x in numbers_list:
