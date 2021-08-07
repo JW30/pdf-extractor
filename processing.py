fe4a73ff82579bf729d7962b325ef80c8356ba60
JW30 <46195508+JW30@users.noreply.github.com>
2021-08-07 10:38:51 +0200

    1. record.txt added to track usage and extended processing.py accordingly 2. Added to output: amount of product numbers found

--- processing.py
+++ processing.py
@@ -1,16 +1,35 @@
 import textract
 import re
+from datetime import datetime
 
 
 def get_numbers(file_path):
-    output = ''
+    # Get PDF content and filter for 8 digit numbers
+    output = []
     try:
         pdf_content = textract.process(file_path)
     except UnicodeDecodeError:
-        pdf_content = textract.process(file_path, method="pdfminer")
+        pdf_content = textract.process(file_path, method='pdfminer')
     text = str(pdf_content)
     numbers_list = re.sub('\D', ' ', text).split()
     for x in numbers_list:
         if len(x) == 8 and x not in output:
-            output += f'{x} '
-    return output.rstrip()
+            output.append(x)
+    numbs_amnt = len(output)
+
+    # Get date, amount of processed PDF files and extracted numbers
+    # and write to record.txt
+    now = datetime.now()
+    dt_string = now.strftime('%d/%m/%Y %H:%M:%S')
+    with open('record.txt', 'r') as f:
+        lines = f.readlines()
+        amnt = int(''.join(re.sub('\D', ' ', lines[0]).split()))
+        amnt += 1
+        curr_numbs = int(''.join(re.sub('\D', ' ', lines[1]).split()))
+        curr_numbs += numbs_amnt
+    with open('record.txt', 'w') as f:
+        f.write(f'PDF files processed: {amnt}\n'
+                f'Product numbers extracted: {curr_numbs}\n'
+                f'Last time used: {dt_string}')
+
+    return f'{numbs_amnt} product numbers found: ' + ' '.join(output)
